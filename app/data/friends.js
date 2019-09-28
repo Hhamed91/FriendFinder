82d08dc37690200bff254af92979fcbb5daf9180
user.name <hazem.hamed.h@hotmail.com>
2019-09-28 08:58:09 -0700

    add new friends to js file for testing

---
+++ app/data/friends.js
@@ -0,0 +1,277 @@
+var friends = [
+    {
+        "name": "Isaac Newton",
+        "photo": "/assets/images/tbd",
+        "scores": [
+             3,
+             3,
+             5,
+             2,
+             5,
+             3,
+             5,
+             2,
+             5,
+             1
+        ]
+   },
+   {
+        "name": "Albert Einstein",
+        "photo": "/assets/images/tbd",
+        "scores": [
+             2,
+             1,
+             5,
+             1,
+             5,
+             5,
+             4,
+             3,
+             2,
+             5
+        ]
+   },
+   {
+        "name": "Che Guevara",
+        "photo": "/assets/images/tbd",
+        "scores": [
+             5,
+             5,
+             5,
+             5,
+             2,
+             5,
+             1,
+             5,
+             3,
+             2
+        ]
+   },
+   {    
+        "name": "Oprah Winfrey",
+        "photo": "/assets/images/tbd",
+        "scores": [
+             1,
+             5,
+             3,
+             2,
+             5,
+             1,
+             2,
+             4,
+             5,
+             1
+        ]
+   },
+   {  
+        "name": "Ellen DeGeneres",
+        "photo": "/assets/images/tbd",
+        "scores": [
+             5,
+             1,
+             1,
+             2,
+             2,
+             5,
+             4,
+             1,
+             3,
+             4
+        ]
+   },
+   {
+        "name": "Julius Caesar",
+        "photo": "/assets/images/tbd",
+        "scores": [
+             2,
+             2,
+             3,
+             2,
+             1,
+             5,
+             4,
+             1,
+             2,
+             2
+        ]
+   },
+   {
+        "name": "Miley Cyrus",
+        "photo": "/assets/images/tbd",
+        "scores": [
+             2,
+             1,
+             5,
+             1,
+             4,
+             5,
+             2,
+             1,
+             3,
+             3
+        ]
+   },
+   {
+        "name": "Goku",
+        "photo": "/assets/images/artlover-man.jpg",
+        "scores": [
+             5,
+             5,
+             4,
+             5,
+             2,
+             3,
+             2,
+             1,
+             2,
+             4
+        ]
+   },
+   {
+        "name": "Walter White",
+        "photo": "/assets/images/hollywood.jpg",
+        "scores": [
+             1,
+             1,
+             5,
+             2,
+             2,
+             5,
+             1,
+             3,
+             4,
+             1
+        ]
+   },
+   {
+        "name": "Michael Jordan",
+        "photo": "assets/images/likes-lavender.jpg",
+        "scores": [
+             5,
+             5,
+             5,
+             5,
+             1,
+             1,
+             1,
+             1,
+             2,
+             4
+        ]
+   },
+   {
+        "name": "Donald Trump",
+        "photo": "/assets/images/scubadiver.jpg",
+        "scores": [
+             2,
+             1,
+             4,
+             1,
+             5,
+             3,
+             5,
+             2,
+             5,
+             1
+        ]
+   },
+   { 
+        "name": "Sarah Palin",
+        "photo": "assets/images/likes-dogs-woman.jpg",
+        "scores": [
+             3,
+             4,
+             3,
+             3,
+             3,
+             3,
+             4,
+             2,
+             2,
+             4
+        ]
+   },
+   {
+        "name": "Lady Gaga",
+        "photo": "assets/images/meditator-man.jpg",
+        "scores": [
+             5,
+             3,
+             1,
+             2,
+             1,
+             5,
+             1,
+             1,
+             1,
+             4
+        ]
+   },
+   { 
+        "name": "Hulk Hogan",
+        "photo": "assets/images/outdoorsman.jpg",
+        "scores": [
+             5,
+             5,
+             2,
+             3,
+             1,
+             5,
+             4,
+             1,
+             1,
+             2
+        ]
+   },
+   {
+       //Wade Moore
+        "name": "Kim Jong-il",
+        "photo": "assets/images/selfie.jpg",
+        "scores": [
+             2,
+             2,
+             5,
+             3,
+             4,
+             4,
+             4,
+             1,
+             4,
+             1
+        ]
+   },
+   {
+        "name": "Macho Man",
+        "photo": "/assets/images/shopper.jpg",
+        "scores": [
+             4,
+             5,
+             4,
+             3,
+             2,
+             4,
+             1,
+             2,
+             5
+        ]
+   },
+   {
+        "name": "Beethoven",
+        "photo": "/assets/images/car-man.jpg",
+        "scores": [
+             5,
+             3,
+             5,
+             4,
+             5,
+             5,
+             2,
+             2,
+             1,
+             3
+        ]
+   }
+];
+
+
+module.exports = friends;
