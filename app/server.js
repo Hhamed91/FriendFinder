8eff4ba855e99f3c2e41170ceab8ea1a4630e261
user.name <hazem.hamed.h@hotmail.com>
2019-09-28 08:44:27 -0700

    Set up server.js file to require path and join the pwd of public file and listen to server 3000

--- app/server.js
+++ app/server.js
@@ -4,3 +4,20 @@ var path = require("path");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '/app/public')));
+
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+
+
